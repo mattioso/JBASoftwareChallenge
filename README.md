ef4898a88c8fc40ee23a936712b7b45225304593
Matthew Stapleton <ofnofixedabode@hotmail.com>
2020-03-09 14:30:21 +0000

    Added Reader to take grid references and place values in csv file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # JBASoftwareChallenge
 A repo to hold the code for the JBA software challenge
+
+The provided data had inconsistencies, with how the data was written, so I manually normalized it
+Grid values changed: (28, 222), (49, 221), (49, 222), (50, 221), (107, 280)
+Data given lacked spacing between months, I extrapolated, based on the style and format of the file where the spaces between months would go, and placed them in manually
+In a real situation, I would ask the creator of the data to send me the correct data for these grid values. I modified the data in order to move forward with the project knowing that it would be easy to retrospectively change the data later on in the project
+
+On my computer, the solution takes 1.303 seconds.
+It generates a comma separated file which can easily be converted to a spreadsheet, or used elsewhere
--- Version1/Main.py
+++ Version1/Main.py
@@ -1,7 +1,6 @@
-
 class Reader(object):
 
-  def __init__(self, fileName):
+  def __init__(self, fileName, databasePath):
 
     self.file = open(fileName, 'r')
     self.fileLines = self.file.readlines()
@@ -9,12 +8,42 @@ class Reader(object):
     self.years = self.getYears()
     self.fileLines = self.fileLines[5:]
 
-    print(self.fileLines)
+    self.file = open(databasePath, "w")
+
+    self.currentPos = []
+    self.currentYear = int(self.years[0])
+    self.prevLine = None
+    self.currentLine = None
+
+    self.file.write("Xref,Yref,Date,Value\n")
+
+    for line in self.fileLines:
+      if "Grid-ref=" in line:
+        self.currentPos = list(filter(None, line.split(" ")[1:]))
+        self.currentPos = [self.currentPos[0][:-1], self.currentPos[1].rstrip("\n")]
+        self.currentYear = int(self.years[0])
+
+      else:
+        self.prevLine = self.currentLine
+        self.currentLine = list(filter(None, line.split(" ")))
+        try:
+          self.currentLine[11] = self.currentLine[11].rstrip("\n")
+
+        except:
+          print("{}, {}".format(self.currentPos[0], self.currentPos[1]))
+
+        for month in range(len(self.currentLine)):
+          self.file.write("{},{},{}/1/{},{}\n".format(self.currentPos[0], self.currentPos[1], month+1, self.currentYear, self.currentLine[month]))
+
+        self.currentYear += 1
+
+    self.file.close()
 
   def getYears(self):
-    # self.years = self.fileLines[4].split("[")
-    # self.years = self.years[2][:-2]
-    # self.years = self.years.split("=").pop(1).split("-")
     return self.fileLines[4].split("[")[2][:-2].split("=").pop(1).split("-")
 
-R = Reader("Version1/cru-ts-2-10.1991-2000-cutdown.pre")
+filePath = ""
+databasePath = ""
+
+Reader("cru-ts-2-10.1991-2000-cutdown-modified.pre", "Database.csv")
+
